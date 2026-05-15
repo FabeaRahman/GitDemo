0f5785dc86ba2f202792f167cab01905d1b41599
Kazi Fabia Rahman <fabearahman@gmail.com>
2026-05-15 22:00:53 +0600

    Updated rules

--- index.py
+++ index.py
@@ -1,2 +1,20 @@
-print("Hello Git")
-print("Fabea")
+# to open a folder direct in desktop
+print (" first type 'cd Desktop' then 'mkdr ' folder name' ' ")
+# to turn the folder into git repo
+print(" type in git bash terminal - 'git init' ")
+# to go to a folder 
+print (" type 'cd Desktop' then 'cd 'folder name' ' ")
+# to run a file in a folder by git bush
+print (" first type file type then file name , like : 'python index.py' ")
+# to edit the file 
+print(" type 'notepad index.py' or 'code index.py' or to open the full folder in vs code 'code . '")
+# to check
+print (" type 'git status ' in the terminal")
+# to save
+print (" type 'git add .' ")
+# to commit 
+print ("type ' git commit -m 'the comment you want to add' ")
+# to push in github real repo
+print (" type 'git push or git push origin main ' in the vs code terminal or git bash terminal")
+# to check the diff between the past edit and new 
+print (" type 'git diff ' ")
