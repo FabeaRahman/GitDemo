5ace2298d7ada0985197dd57591b05f564382564
Kazi Fabia Rahman <fabearahman@gmail.com>
2026-05-16 15:01:27 +0600

    main branch edit

---
+++ Github_and_Git_practice/branch.py
@@ -0,0 +1,13 @@
+# then add some new changes in the main part
+# again add and commit
+# to merge the main and sub branch
+print(" type 'git merge feature-greeting'")
+# then again add and commit the changes
+# to delete a branch first check if you are in that branch or not 
+print (" type 'git branch -d 'branch name'")
+# to check the history graph 
+print (" type 'git log --oneline --graph --all ' ")
+# then push the changes in the real repo in github
+print (" type 'git push origin main ' ")
+# to push the sub branch 
+print (" typr ' git push origin 'branch name' ' ")
