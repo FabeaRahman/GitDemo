5b4c1c3db51aace26991c83865702afe4ddf8056
Kazi Fabia Rahman <fabearahman@gmail.com>
2026-05-16 14:53:28 +0600

    Commands - Day 2 -Creating and editing by a new branch

---
+++ Github_and_Git_practice/branch.py
@@ -0,0 +1,13 @@
+# to check which branch is present
+print("type 'git branch'")
+# to create a new branch and switch
+print (" type 'git checkout -b'new branch name' '")
+# to create a branch
+print(" type ' git branch 'branch name' '")
+# to switch to the new branch
+print (" type 'git switch/checkout 'branch name' '")
+#  then open VS Code or notepad to edit a file
+# then add and commit the changes 
+print (" type 'git add branch.py and git commit -m 'Added greeting function' ")
+# to return in main branch
+print ("type 'git checkout main")
