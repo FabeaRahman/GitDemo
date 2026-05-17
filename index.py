43dca5701cfa61dd27af7bfbecbde9a06690c0f3
Kazi Fabea Rahman <fabearahman@gmail.com>
2026-05-17 22:58:16 +0600

    Update rules

--- index.py
+++ index.py
@@ -27,7 +27,7 @@ print("type 'touch .gitignore' ")
 # then go to the editor to edit gitignore by code / notepad
 #__pycache__/ → Python cache folder ignore ,*.pyc → compiled python file ignore ,*.log → log file ignore ,.env → secret/env file ignore ,venv/ → virtual environment ignore 
 # then add commit and puch to the git
-# to download and merge details from GitHub to local pc manually without 'pull'
+# to download and merge details from GitHub to local pc manually without 'pull' using fetch and merge
 print (" type 'git fetch origin'")
 print (" type 'git log main..origin/main {from which branch the changes occurs} --oneline' ")# to check the history of changes in GitHub but still not present in local device
 print (" type 'git merge origin/main '")
